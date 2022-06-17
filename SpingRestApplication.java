5247c0ea194409b373cf8ee72f7fbd4a09b54412
Sagar01079 <107676225+Sagar01079@users.noreply.github.com>
2022-06-17 16:36:12 +0530

    Update SpingRestApplication.java

--- SpingRestApplication.java
+++ SpingRestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpingRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpingRestApplication.class, args);
+		//SpringApplication.run(SpingRestApplication.class, args);
 	}
 
 }
